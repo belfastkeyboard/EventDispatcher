cec8b68112532789ad2243fe674c5f09e21ffc39
riain <ryanftoal@gmail.com>
2024-08-25 04:07:43 +0100

    Implemented basic event dispatcher pattern

---
+++ dispatcher.c
@@ -0,0 +1,56 @@
+#include <assert.h>
+#include <malloc.h>
+#include "event.h"
+#include "table.h"
+#include "dispatcher.h"
+#include "rbuff.h"
+
+typedef struct EventDispatcher
+{
+    Table *table;
+    RingBuffer *rbuff;
+} EventDispatcher;
+
+EventDispatcher *event_dispatcher = NULL;
+
+void create_event_dispatcher(void)
+{
+    event_dispatcher = malloc(sizeof(EventDispatcher));
+    event_dispatcher->table = create_table();
+    event_dispatcher->rbuff = create_ringbuffer(32, sizeof(Event));
+}
+void destroy_event_dispatcher(void)
+{
+    assert(event_dispatcher);
+
+    destroy_table(&event_dispatcher->table);
+    destroy_ringbuffer(&event_dispatcher->rbuff);
+
+    free(event_dispatcher);
+    event_dispatcher = NULL;
+}
+
+void register_listener(Event event, EventHandler handler)
+{
+    table_insert(event_dispatcher->table, event, handler);
+}
+void submit_event(Event event)
+{
+    assert(event_dispatcher);
+
+    if (!full(event_dispatcher->rbuff))
+        write(event_dispatcher->rbuff, &event);
+}
+void dispatch_events(void)
+{
+    while (!empty(event_dispatcher->rbuff))
+    {
+        Event event = *(Event*)read(event_dispatcher->rbuff);
+        EventHandler handler = table_get(event_dispatcher->table, event);
+
+        if (handler)
+            handler();
+    }
+
+    clear(event_dispatcher->rbuff);
+}
---
+++ dispatcher.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "event.h"
+
+void create_event_dispatcher(void);
+void destroy_event_dispatcher(void);
+
+void register_listener(Event event, EventHandler handler);
+void submit_event(Event event);
+void dispatch_events(void);
